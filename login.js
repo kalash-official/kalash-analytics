5b6bc473ab292b9afe8ae529398c778f299e92c2
kalash-official <princesingh00037@gmail.com>
2026-04-08 11:46:38 +0530

    Create login.js

---
+++ login.js
@@ -0,0 +1,38 @@
+const phoneInput = document.querySelector("input");
+const btn = document.querySelector(".btn");
+const loading = document.getElementById("loading");
+
+// Recaptcha
+window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container', {
+  size: 'invisible'
+});
+
+// Click
+btn.addEventListener("click", sendOtp);
+
+function sendOtp() {
+  let phone = phoneInput.value.trim();
+
+  if (phone === "" || phone.length < 10) {
+    alert("Enter valid phone");
+    return;
+  }
+
+  loading.style.display = "flex";
+  btn.disabled = true;
+
+  let fullPhone = "+91" + phone;
+
+  firebase.auth().signInWithPhoneNumber(fullPhone, window.recaptchaVerifier)
+    .then((confirmationResult) => {
+
+      window.confirmationResult = confirmationResult;
+      window.location.href = "otp.html";
+
+    })
+    .catch((error) => {
+      alert(error.message);
+      loading.style.display = "none";
+      btn.disabled = false;
+    });
+}
